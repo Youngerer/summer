1756516ee6c8d84043afa2699d197b30a8b72bba
unknown <31477@JFHN-PC.goldwind.org>
2017-04-26 16:54:22 +0800

    index 脚本修改

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
-function myconsole()
+﻿function myconsole()
 {
   alert('咨询已提交');
 
 }
+
+        function getupload() {
+            var code = document.getElementById("code").value;
+            var phone = document.getElementById("phone").value;
+            if (code !=""&& phone !="") {
+                alert('商品编号：'+code+',电话：'+phone+',订单成功！'+',联系电话：17310557925');
+            }
+            else {
+                alert('填写完整订单');
+            }
+        }
