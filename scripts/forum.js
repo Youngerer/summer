ca6c340bee1986c5ab7d0f7d4c5abb8c67aa06cd
unknown <31477@JFHN-PC.goldwind.org>
2017-04-27 11:12:31 +0800

    indexhtml and forumjs

---
+++ scripts/forum.js
@@ -0,0 +1,9 @@
+﻿
+
+function forumadd() {
+ 
+    var phone = document.getElementById('phone');
+    var content = document.getElementById('content');
+    var date = new Date(Date.now);
+    alert(phone+'aaaaaaaaa'+conetent+'+++'+date);
+}
