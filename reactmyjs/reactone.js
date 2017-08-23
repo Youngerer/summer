68256db94a90e8a46a06955bd88bc351981cbd9a
unknown <songyang@goldwind.com.cn>
2017-08-23 10:05:34 +0800

    reactpages

---
+++ reactmyjs/reactone.js
@@ -0,0 +1,6 @@
+var HelloMessage = React.createClass({
+    displayName: 'HelloMessage',
+    render: function render() {
+        return React.createElement('div', null, 'Hello ', this.props.name);
+    }
+});
